1a086fe9a8aefa0123f1411e1eb6ce9df0f5200f
Benjamin Crews <aceF22@gmail.com>
2018-08-05 21:04:16 -0500

    Minor Changes, new bugs
    Added pyperclip.
    Fixed newRecipe.
    newRecipe may need to be decorated to take full advantage of pyperclip.
    Issues #5 & #6 discovered.

--- Py4Dinner.py
+++ Py4Dinner.py
@@ -9,7 +9,8 @@ import json
 import os
 from recipe_scrapers import scrape_me
 import re
-logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - ' +\
+import pyperclip
+logging.basicConfig(level=logging.INFO, format=' %(asctime)s - ' +\
                     '%(levelname)s - %(message)s')
 
 os.chdir('D:\\Czarified\\Documents\\GitHub\\py4Dinner')
@@ -193,7 +194,7 @@ def listFood(book):
         for i in book:
             print(i.name)
             
-def newRecipe(url):
+def newRecipe(url=pyperclip.paste()):       # Needs to be decorated???
     '''
     Scrapes the given webpage of all required recipe data and returns a
     new Food class with appropriate attributes.
@@ -229,12 +230,12 @@ def newRecipe(url):
     regNum = re.compile(r'''(
         (\d+/?\d*)                       # The ingredient quantity, Grp1
         \s?                              # Separator
-        (teaspoon|tablespoons|           # Measurements           , Grp2
+        (teaspoon|tablespoon|            # Measurements           , Grp2
          fluid oz|cup|pint|quart|
          gallon|milliliter|millilitre|
          liter|litre|pound|ounce|
          milligram|gram|kilogram|
-         millimeter|centimeter|meter|inch)?
+         millimeter|centimeter|meter|inch)?s?
         \s?                         # Separator
         (.*)                        # The actual ingredient , Grp3
         )''', re.VERBOSE)
@@ -242,13 +243,16 @@ def newRecipe(url):
     for i in scraps:                    # Take the regex match object
         mo = regNum.findall(i)          # and format it for the Food Class.
         # logging.debug(mo)
-        tmpFood.ingr.append(list(mo[0][1:]))
+        if mo == []:
+            continue
+        else:
+            tmpFood.ingr.append(list(mo[0][1:]))
         
     for i in tmpFood.ingr:
-        logging.debug('')
+        # logging.debug('')
         numstr = i[0]
         if numstr.isnumeric():
-            i[0] = float(numstr)            # Convert the string numbers
+            i[0] = float(numstr)            # Convert the string numbers           
         else:
             numl = numstr.split('/')
             i[0] = float(numl[0])/float(numl[1])
